25c45c108c15d08ebaa8d598ae2447f9178f5c9a
tanguyhardion <45904983+tanguyhardion@users.noreply.github.com>
2022-11-16 15:09:07 +0100

    Correction type crédits (int)

--- src/StrategieOffensive.java
+++ src/StrategieOffensive.java
@@ -10,7 +10,7 @@ public class StrategieOffensive implements StrategieEtudiant {
 	 * Attaque un autre etudiant en lui enelevant des credits.
 	 * 
 	 * @param origine l'etudiant qui attaque
-	 * @param cible l'etudiant qui est attaque
+	 * @param cible   l'etudiant qui est attaque
 	 */
 	public void agir(Etudiant origine, Etudiant cible) {
 
@@ -19,10 +19,8 @@ public class StrategieOffensive implements StrategieEtudiant {
 		if (x <= (40 + 3 * origine.getDexterite())) {
 			double y = Math.random();
 			double coeffDegat = Math.max(0, Math.min(100, 10 * origine.getForce() - 5 * cible.getResistance()));
-			cible.removeCredits(Math.floor(y * (1 + coeffDegat) * 10));
+			cible.removeCredits((int) Math.floor(y * (1 + coeffDegat) * 10));
 		}
-
-		throw new UnsupportedOperationException();
 	}
 
 }
